02d2e2906bd2be95dda266761661a3bf69ef414a
Arun MR <arunmr@beaverlodge.in>
2020-05-29 12:07:55 +0530

    string test added

---
+++ python/strtest.py
@@ -0,0 +1,6 @@
+inp_str = "Hello World"
+test_str = input("Enter test string\n")
+if(test_str in inp_str):
+   print(test_str + ' is present in ' + inp_str)
+else:
+   print(test_str + ' is not present in ' + inp_str)
