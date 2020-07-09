ed305d2dbf9be9204995b931ef89c5e7ed4bc437
Arun MR <arunmr@beaverlodge.in>
2020-07-09 21:05:12 +0530

    added flask project dir

---
+++ python/flask/src/flaskapp.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+   x={'numbers':[1,2,3,4]}
+   return jsonify(x)
+
+@app.route('/hai')
+def hai():
+   return 'hai'
+
+if __name__ == '__main__':
+   app.run()
