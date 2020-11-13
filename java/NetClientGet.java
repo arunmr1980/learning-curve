a2fde693229cfa2f70ebb65f3745f9210d5b0a2d
Arun MR <merry.arun@gmail.com>
2020-11-13 17:10:28 +0530

    added NetGetClient

---
+++ java/NetClientGet.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetClientGet {
+
+    // http://localhost:8080/RESTfulExample/json/product/get
+    public static void main(String[] args) {
+
+      try {
+
+        URL url = new URL("https://5vu8c1qf95.execute-api.us-west-2.amazonaws.com/dev/leave/8dc1ffdb77fae3440c5f9d950849dd0b");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+            (conn.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+
+      } catch (MalformedURLException e) {
+
+        e.printStackTrace();
+
+      } catch (IOException e) {
+
+        e.printStackTrace();
+
+      }
+
+    }
+
+}
